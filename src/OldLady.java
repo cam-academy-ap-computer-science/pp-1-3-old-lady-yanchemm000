7136d2474e4a0b515655198e3a4da72b9f42106a
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-16 07:53:36 -0700

    still needs work

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,7 +3,13 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		oldladyfly();
 
+	}
+	public static void oldladyfly() {
+		System.out.println("There was an old lady who swalled a fly.");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
 	}
 
 }

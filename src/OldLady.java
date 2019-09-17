78270b4f09635039703c4e8b90c5afc5f08dc594
TouchSmart <TouchSmart@192.168.0.16>
2019-09-16 17:45:52 -0700

    done at home

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,13 +3,56 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		there();
+		System.out.println("fly.");
 		oldladyfly();
-
+		System.out.println();
+		there();
+		System.out.println("spider,\nThat wriggled and iggled and jiggled inside her.");
+		swallowed1();
+		oldladyfly();
+		System.out.println("");
+		there();
+		System.out.println("bird,\nHow absurd to swallow a bird.");
+		swallowed2();
+		swallowed1();
+		oldladyfly();
+		System.out.println("");
+		there();
+		System.out.println("cat,\nImagine that to swallow a cat.");
+		swallowed3();
+		swallowed2();
+		swallowed1();
+		oldladyfly();
+		System.out.println("");
+		there();
+		System.out.println("dog,\nWhat a hog to swallow a dog.");
+		swallowed4();
+		swallowed3();
+		swallowed2();
+		swallowed1();
+		oldladyfly();
+		System.out.println("");
+		there();
+		System.out.println("horse,\nShe died of course.");
 	}
 	public static void oldladyfly() {
-		System.out.println("There was an old lady who swalled a fly.");
 		System.out.println("I don't know why she swallowed that fly,");
 		System.out.println("Perhaps she'll die.");
 	}
-
+	public static void there() {
+		System.out.print("There was an old lady who swallowed a ");
+	}
+	public static void swallowed1() {
+		System.out.println("She swallowed the spider to catch the fly,");
+	}
+	public static void swallowed2() {
+		System.out.println("She swallowed the bird to catch the spider,");
+	}
+	public static void swallowed3() {
+		System.out.println("She swallowed the cat to catch the bird,");
+	}
+	public static void swallowed4() {
+		System.out.println("She swallowed the dog to catch the cat,");
+	}
 }
